2a02cabfc17aee1c24003cbb1f4fbc6d64d9c6e5
Maarten Trompper <maartentrompper@freedom.nl>
2017-09-10 18:05:31 +0200

    fix typing

--- src/abstract-expression/expression.ts
+++ src/abstract-expression/expression.ts
@@ -5,3 +5,13 @@ export interface Expression<T> {
     resolve(): T;
 }
 
+export class Identifity<T> implements Expression<T> {
+    readonly x: T;
+
+    constructor(x: T) {
+        this.x = x;
+    }
+    resolve(): T {
+        return this.x;
+    }
+}
